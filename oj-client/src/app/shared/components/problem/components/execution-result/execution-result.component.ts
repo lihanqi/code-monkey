f1e02124c3539f51981cb0a51031980d6259b110
Hanqi Li <hanqi.derek.li@gmail.com>
2018-03-06 18:25:33 -0800

    add loading indicator for loading problem list

---
+++ oj-client/src/app/shared/components/problem/components/execution-result/execution-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-execution-result',
+  templateUrl: './execution-result.component.html',
+  styleUrls: ['./execution-result.component.css']
+})
+export class ExecutionResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
