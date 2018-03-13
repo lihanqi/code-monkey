c03521cc59ac843d3094f21d07cec0ebd0a8d8cb
Hanqi Li <hanqi.derek.li@gmail.com>
2018-03-12 22:26:22 -0700

    remove some files-follow up

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -225,7 +225,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = __webpack_require__("../../../core/esm5/core.js");
-var data_service_1 = __webpack_require__("../../../../../src/app/services/data/data.service.ts");
+var data_service_1 = __webpack_require__("../../../../../src/app/problem/services/data/data.service.ts");
 var problem_1 = __webpack_require__("../../../../../src/app/shared/models/problem.ts");
 var NewProblemComponent = /** @class */ (function () {
     function NewProblemComponent(dataService) {
@@ -1326,76 +1326,6 @@ var ProfileModule = /** @class */ (function () {
     return ProfileModule;
 }());
 exports.ProfileModule = ProfileModule;
-
-
-/***/ }),
-
-/***/ "../../../../../src/app/services/data/data.service.ts":
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = __webpack_require__("../../../core/esm5/core.js");
-var http_1 = __webpack_require__("../../../common/esm5/http.js");
-var ErrorObservable_1 = __webpack_require__("../../../../rxjs/_esm5/observable/ErrorObservable.js");
-var operators_1 = __webpack_require__("../../../../rxjs/_esm5/operators.js");
-var httpOptions = {
-    headers: new http_1.HttpHeaders({ "Content-Type": "application/json" })
-};
-var DataService = /** @class */ (function () {
-    function DataService(http) {
-        this.http = http;
-        this.url = "api/v1/problems";
-    }
-    // todo: may be can optimized for tracking/ lazy-loding purpose;
-    DataService.prototype.getProblems = function () {
-        var _this = this;
-        return this.http
-            .get(this.url)
-            .pipe(operators_1.tap(function (problems) { return (_this.problems = problems); }));
-    };
-    DataService.prototype.getProblemById = function (id) {
-        return this.http
-            .get(this.url + "/" + id)
-            .pipe(operators_1.catchError(this.handleError));
-    };
-    DataService.prototype.addProblem = function (problem) {
-        var _this = this;
-        return this.http.post(this.url, problem, httpOptions).pipe(operators_1.tap(function (problem) {
-            console.log("get called" + _this.problems.length);
-            _this.problems.push(problem);
-        }), operators_1.catchError(this.handleError));
-    };
-    DataService.prototype.handleError = function (error) {
-        if (error.error instanceof ErrorEvent) {
-            // A client-side or network error occurred. Handle it accordingly.
-            console.error("An error occurred:", error.error.message);
-        }
-        else {
-            // The backend returned an unsuccessful response code.
-            // The response body may contain clues as to what went wrong,
-            console.error("Backend returned code " + error.status + ", " + ("body was: " + error.error));
-        }
-        // return an ErrorObservable with a user-facing error message
-        return new ErrorObservable_1.ErrorObservable("Something bad happened; please try again later.");
-    };
-    DataService = __decorate([
-        core_1.Injectable(),
-        __metadata("design:paramtypes", [http_1.HttpClient])
-    ], DataService);
-    return DataService;
-}());
-exports.DataService = DataService;
 
 
 /***/ }),
